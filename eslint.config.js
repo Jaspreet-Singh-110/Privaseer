a9ce00e15f8e96da93e6bd68294c605772642fd4
Jaspreet Singh <Jaspreetsingh1024@outlook.com>
2025-11-22 10:59:15 +0000

    Refactor ESLint configuration to use @typescript-eslint packages and update dependencies in package.json and package-lock.json

--- eslint.config.js
+++ eslint.config.js
@@ -2,18 +2,22 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
 
-export default tseslint.config(
+export default [
   { ignores: ['dist'] },
+  js.configs.recommended,
+  ...tseslint.configs['flat/recommended'],
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
+      parser: tsparser,
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
+      '@typescript-eslint': tseslint,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
@@ -25,4 +29,4 @@ export default tseslint.config(
       ],
     },
   }
-);
+];
